b87d418bf24b4e0505a6366a5fe59c250d33ba1d
Alvsch <94403567+Alvsch@users.noreply.github.com>
2022-03-11 14:48:10 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self, id):
+        self.id = id
+        self.ready = False
+        self.p1Went = False
+        self.p2Went = False
+        self.sentAnswer = False
+        self.winner = -1
+        self.wins = [0, 0]
+        self.answers = [0, 0]
+
+    def get_answers(self, p):
+        pass
